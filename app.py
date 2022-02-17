0bff27cb3899c318ce1ce85499344b8b8321a276
Skyler Clark <skylerjaneclark@gmail.com>
2022-02-17 17:05:29 -0500

    finally figures out how to use the api

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import yaml
 import requests 
+from bs4 import BeautifulSoup
 
 def main():
-    with open("config.yaml", "r") as file: 
+    with open("config.yaml", "r") as file:
         config = yaml.safe_load(file)[0]
-        print(config["query"].split()) 
         query = []
-        for item in config["query"].split(): 
-            item = item.replace(":", "%3A")
+        for item in config["query"].split():
             item = item.replace("/", "%2F")
             query.append(item)
-        query = "+".join(query)
-        print(query)
+        query = "%20".join(query)
         board_name = config["columns"][0]["name"]
-        request = requests.get('https://github.com/orgs/kubernetes/projects/49/search_results?'+ board_name +'=show&q='+ query)
-        print(request.content)
-
+        request_url = 'https://api.github.com/search/issues?q=' + query
+        request = requests.get(request_url)
 main()
